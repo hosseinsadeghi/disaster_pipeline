0476a62df144d950f1db09f4fe9879e2fba82607
Hossein Sadeghi <hse1984@gmail.com>
2020-05-04 09:27:07 +0000

    Add docstring

--- data/process_data.py
+++ data/process_data.py
@@ -2,11 +2,33 @@ import sys
 from sqlalchemy import create_engine
 import pandas as pd
 
+
 def load_data(messages_filepath, categories_filepath):
+    """
+    
+    Parameters:
+    messages_filepath (str): The path to the messages csv file
+    categories_filepath (str): The path to the categories csv file
+    
+    Returns:
+    pandas.DataFrame: A dataframe from the merge of the two csv files
+    """
     messages = pd.read_csv(messages_filepath).drop('original', axis=1)
     categories = pd.read_csv(categories_filepath)
     df = pd.merge(messages, categories)
+    return df
 
+
+
+def clean_data(df):
+    """
+    
+    Parameters:
+    df (pandas.DataFrame): The dataframe
+    
+    Returns:
+    pandas.DataFrame: The cleaned dataframe
+    """
     # create a dataframe of the 36 individual category columns
     categories = categories['categories'].str.split(';', expand=True)
     columns = [x.split('-')[0] for x in categories.values[0]]
@@ -17,11 +39,6 @@ def load_data(messages_filepath, categories_filepath):
 
     # concatenate the original dataframe with the new `categories` dataframe
     df = pd.concat((df.drop('categories', axis=1), categories), axis=1)
-    return df
-    
-
-
-def clean_data(df):
     # load data from database
     df.drop_duplicates(inplace=True)
     df = df.loc[df.isnull().mean(1) < 0.5]
@@ -30,6 +47,13 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    """
+    Function that saves the df in a database. Note, if the table exist, it will be replaced.
+    Parameters:
+    df (pandas.DataFrame): dataframe to be saved in a database
+    database_filename: The path to the database
+    """
+    database_filename = database_filename.lstrip('sqlite:///')
     engine = create_engine('sqlite:///%s' % database_filename)
     engine.execute("DROP TABLE IF EXISTS message_categories;")
     df.to_sql('message_categories', engine, index=False)  
